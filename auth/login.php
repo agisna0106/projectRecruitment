c55a0aacb3b9a8fc9f1ff825da63517dce5e8a78
agisna0106 <sarifan176@gmail.com>
2025-06-18 18:23:15 +0700

    add: login logic

--- auth/login.php
+++ auth/login.php
@@ -2,31 +2,34 @@
 
 require_once '../model/Database.php';
 
-class Login {
-    public $db;
+class Login
+{
+    // public $db;
 
-    public function __construct()
-    {
-        $this->db = new Database();
-    }
+    // public function __construct()
+    // {
+    //     $this->db = new Database();
+    // }
 
-    public function login() 
-    {
-        $username = $_POST['username'];
-        $password = $_POST['password'];
+    // public function login()
+    // {
+    //     $username = $_POST['username'];
+    //     $pass = $_POST['password'];
 
-        $get_password = "SELECT username, password FROM user WHERE username='$username'";
-        $result = $this->db->mysqli->prepare($get_password);
-        if($result == null) {
-            header("Location: loginForm.php");
-            exit;
-        }
-        if($username == $get_password['username'] AND $password == $get_password['password']) {
-            header("Location: ../view/kopi.php");
-            exit;
-        } else {
-            header("Location: loginForm.php");
-            exit;
-        }
-    }
+    //     $res = "SELECT username, password FROM user WHERE username='$username'";
+    //     $result = $this->db->mysqli->prepare($get_password);
+    //     if ($result == null) {
+    //         header("Location: loginForm.php");
+    //         exit;
+    //     }
+    //     if ($username == $get_password['username'] and $pass == $get_password['password']) {
+    //         header("Location: ../view/kopi.php");
+    //         exit;
+    //     } else {
+    //         header("Location: loginForm.php");
+    //         exit;
+    //     }
+    // }
+
+    // public function logout() {}
 }
